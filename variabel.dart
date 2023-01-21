1a396647002ea9c5273711a6c27dc9e740538b9a
Faldiansyah <faldiansyahk@gmail.com>
2023-01-21 22:00:54 +0800

    Initial commit

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main() {
+  // membuat variabel dengan tipe
+  String nama = "Faldi";
+  int umur = 20;
+  double tinggi = 165.00;
+  bool isMenikah = false;
+
+  // membuat variabel dengan kata kunci var
+  var alamat = "Palu, Indonesia";
+
+  // mencetak variabel
+  print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+  print("Menikah : $isMenikah");
+  print("Alamat : $alamat");
+}
